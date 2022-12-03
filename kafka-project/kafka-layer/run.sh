77ee6ca56db9c86df2d83e569b808ca14f6ae9f1
Sergen Çepoğlu <dev.csgn@gmail.com>
2022-12-03 21:28:36 +0300

    Delete run.sh

--- kafka-project/kafka-layer/run.sh
+++
@@ -1,9 +0,0 @@
-
-
-docker network create kafka-tier --driver bridge
-
-
-docker run -d --name zookeeper-server \
- 	--network app-tier \
- 	-e ALLOW_ANONYMOUS_LOGIN=yes \
- 	bitnami/zookeeper:latest
